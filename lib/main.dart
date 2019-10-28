49a6907067d945f461a6ac95d84d2fee8da0d1f7
Felipe Lourenço <felipel@impacto.corp.docspider.com.br>
2019-10-28 09:05:09 -0300

    Removing pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,3 @@
 import 'package:flutter/material.dart';
 
 void main()  => runApp(MyApp());
-
-class MyApp extends StatelessWidget(){
-  @override
-  Widget build(BuildContext context){
-    return MAterialApp(
-      title: 'Login',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.Blue,
-      ),
-      home: LoginPage(),
-    ),
-  }
-}
