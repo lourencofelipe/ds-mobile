7d07ed5ca2be94e264161572e4c940943c6f484c
Felipe Lourenço <felipel@impacto.corp.docspider.com.br>
2019-10-28 08:59:14 -0300

    Adding Main Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main();
+void main()  => runApp(MyApp());
+
+class MyApp extends StatelessWidget(){
+  @override
+  Widget build(BuildContext context){
+    return MAterialApp(
+      title: 'Login',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.Blue,
+      ),
+      home: LoginPage(),
+    ),
+  }
+}
